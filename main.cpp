75e69a43108761a489512f056b2d17992845c927
justin.sneddon <justin.sneddon@gmail.com>
2017-08-03 17:26:40 +0100

    example of slow code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
-int main()
-{
-    std::cout<<"yep!!"<<std::endl;
+//really slow to compile example preprocessor boom!
+//make a 400 meg is exe
+
+#define a "xxxxxxxxxxx"
+#define b a a a a a a a
+#define c b b b b b b b
+#define d c c c c c c c
+#define e d d d d d d d
+#define f e e e e e e e
+#define g f f f f f f f
+#define h g g g g g g g
+#define i h h h h h h h
+#define j i i i i i i i
+const char*buff=j;
+
+int main(void){
+
     return 0;
 }
